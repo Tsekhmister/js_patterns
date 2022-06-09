58a12cf85cddebc71386347233f2e0a5bf4b2bd0
Sergii Tsekhmister <tsehmistersergey@gmail.com>
2022-06-09 19:38:53 +0300

    added two files for test

---
+++ script.js
@@ -0,0 +1,26 @@
+// "use strict"
+// const comments = [];
+
+// document.querySelector('button').onclick = () => {
+//   let value = document.querySelector('input').value;
+//   comments.push(value);
+//   draw();
+// }
+
+// const draw = () => {
+//   let out = '';
+
+//   for (let i = 0; i < comments.length; i++) {
+//     out += `<li>${comments[i]}</li>`;
+//   }
+
+//   document.querySelector('ul').innerHTML = out;
+// } 
+
+// console.log([..."Ulia"])
+
+// function Add(...data) {
+//   return data;
+// }
+
+// console.log(Add(21));
