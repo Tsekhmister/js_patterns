378efafe53b6c71e1a2f95cf949727dbb2e7c25d
Sergii Tsekhmister <tsehmistersergey@gmail.com>
2022-06-09 20:24:01 +0300

    added singleton

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
-// "use strict"
-// const comments = [];
+//Singleton - это порождающий паттерн, который гарантирует что у класса есть только один экземпляр
 
-// document.querySelector('button').onclick = () => {
-//   let value = document.querySelector('input').value;
-//   comments.push(value);
-//   draw();
-// }
+class DataBase {
+  url;
+  static instance;
 
-// const draw = () => {
-//   let out = '';
+ constructor() {
+  if (DataBase.instance) {
+    return DataBase.instance;
+   }
+   this.url = Math.random();
+   DataBase.instance = this;
+ }
+}
 
-//   for (let i = 0; i < comments.length; i++) {
-//     out += `<li>${comments[i]}</li>`;
-//   }
+const db1 = new DataBase();
+const db2 = new DataBase();
 
-//   document.querySelector('ul').innerHTML = out;
-// } 
+console.log(db1)
+console.log(db2)
+console.log(db2 === db1)
 
-// console.log([..."Ulia"])
+//||||||||||||||||||// 
+let instance;
 
-// function Add(...data) {
-//   return data;
-// }
+class Counter {
+  constructor() {
+    if (typeof Counter.instance === 'object') {
+      return Counter.instance;
+    }
+    this.count = 0;
+    Counter.instance = this;
+    return this;
+  }
 
-// console.log(Add(21));
+  getCount() {
+    return this.count;
+  }
+
+  increaseCount() {
+    return this.count++;
+  }
+}
+
+const myCounter1 = new Counter();
+const myCounter2 = new Counter();
+
+myCounter1.increaseCount();
+myCounter1.increaseCount();
+myCounter2.increaseCount();
+myCounter2.increaseCount();
+
+console.log(myCounter1.getCount());
+console.log(myCounter2.getCount());
+console.log(myCounter2 === myCounter1 );
+//
+
+//Factory method - создание класса, который будет помогать создавать определенныеобъекты на основании каких либо данных
